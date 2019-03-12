7c5fffa0dcedf39afa5f286c244802ba954312c4
kavyachinnu <kavya.jc.12377@gmail.com>
2019-03-12 10:50:43 +0530

    addition

---
+++ Add.java
@@ -0,0 +1,7 @@
+public class Add{
+    public static void main(String[] args) {
+        int num1 = 6, num2 = 3, sum;
+        sum = num1 + num2;
+        System.out.println("sum =" +sum);
+    }
+}
